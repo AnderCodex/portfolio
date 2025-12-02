42ca478e2b8d16a7fbbb7f1cdb6e110cc5796d6d
AnderCodex <ander@local>
2025-12-02 14:43:08 -0300

    chore: upgrade to Java 21 (maven-compiler-plugin) and apply OpenRewrite

---
+++ Desktop/mios/java/solicitud_servicios/src/main/java/uy/ander/solicitud_servicios/SolicitudServiciosApplication.java
@@ -0,0 +1,13 @@
+package uy.ander.solicitud_servicios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolicitudServiciosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolicitudServiciosApplication.class, args);
+	}
+
+}
---
+++ Desktop/mios/java/solicitud_servicios/src/test/java/uy/ander/solicitud_servicios/SolicitudServiciosApplicationTests.java
@@ -0,0 +1,13 @@
+package uy.ander.solicitud_servicios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolicitudServiciosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
